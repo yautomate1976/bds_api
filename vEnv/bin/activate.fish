c9943ea221eeded53f6f69a74c5600c709ee5625
root <root@unknowne4a471a121f1.attlocal.net>
2017-10-04 20:13:09 -0700

    updates

--- vEnv/bin/activate.fish
+++ vEnv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/yammonraheem/PycharmProjects/Concur/GitHub/bds_api/vEnv"
+set -gx VIRTUAL_ENV "/home/yammonraheem/dev/GitHub/bds_api/vEnv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
